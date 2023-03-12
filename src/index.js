2fdf48fb4eb9f668afd9c660e179919a14dd28b2
okdohyuk <root@okdohyuk.dev>
2023-03-12 04:30:55 +0000

    feat processing react redux reducer

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './redux/reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 
 const router = createBrowserRouter([
   {
@@ -28,7 +36,13 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+		<Provider store={createStoreWithMiddleware(
+			Reducer,
+			window.__REDUX_DEVTOOLS_EXTENSION__ &&
+			window.__REDUX_DEVTOOLS_EXTENSION__()
+			)} >
+    		<RouterProvider router={router} />
+		</Provider>
   </React.StrictMode>
 );
 
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+// import user from './userReducer';
+// import chatRoom from './chatRoomReducer';
+
+const rootReducer = combineReducers({
+	// user,
+	// chatRoom
+});
+
+export default rootReducer;
