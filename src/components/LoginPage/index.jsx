339574e71e06d1b4fd4d105c25c5d9cd8df0d74f
okdohyuk <root@okdohyuk.dev>
2023-03-12 08:09:48 +0000

    feat register page style

--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function LoginPage() {
-	return <div>login</div>;
+	return (
+		<div className="auth-wrapper" >
+			<div style={{textAlign: 'center'}} >
+				<h3>Login</h3>
+			</div>
+			<form>
+				<label>Email</label>
+      			<input name="email" type="email" />
+				<label>Password</label>
+				<input name="password" type="password" />
+      			<input type="submit" />
+				<Link style={{ color: 'gray', textDecoration: 'none'}} to="/register" >회원가입</Link>
+    		</form>
+		</div>
+	);
 };
 
 export default LoginPage;
