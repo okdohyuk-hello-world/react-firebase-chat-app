990e7a65decca8f5310e3b0b2d4722066fca90ea
okdohyuk <root@okdohyuk.dev>
2023-03-15 11:45:19 +0000

    feat chat page panel component files

---
+++ src/components/ChatPage/MainPanel/index.jsx
@@ -0,0 +1,8 @@
+// TODO 강의자 오류로 일단 클래스 컴포넌트 사용 추후 변경
+import React, { Component } from 'react';
+
+export default class MainPanel extends Component {
+	render() {
+		return <div>MainPanel</div>;
+	};
+};
--- src/components/ChatPage/index.jsx
+++ src/components/ChatPage/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import SidePanel from './SidePanel';
+import MainPanel from './MainPanel';
 
 function ChatPage() {
-	return <div>chat</div>;
+	return (
+		<div style={{display: 'flex'}} >
+			<div style={{width: '300px'}} >
+				<SidePanel />
+			</div>
+			<div style={{width: '100%'}} >
+				<MainPanel />
+			</div>
+		</div>
+	);
 };
 
 export default ChatPage;
