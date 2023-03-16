c94df6a11ed06269168bd0c2d9baf9c0b0eaf720
okdohyuk <root@okdohyuk.dev>
2023-03-16 11:08:22 +0000

    feat chat page component files

--- src/components/ChatPage/MainPanel/index.jsx
+++ src/components/ChatPage/MainPanel/index.jsx
@@ -1,8 +1,28 @@
 // TODO 강의자 오류로 일단 클래스 컴포넌트 사용 추후 변경
 import React, { Component } from 'react';
+import MessageHeader from './MessageHeader';
+// import Message from './Message';
+import MessageForm from './MessageForm';
 
 export default class MainPanel extends Component {
 	render() {
-		return <div>MainPanel</div>;
+		return (
+			<div style={{ padding: '2rem 2rem 0 2rem'}}>
+				<MessageHeader />
+				
+				<div style={{
+						width: '100%',
+						height: '450px',
+						border: '.2rem solid #ececec',
+						borderRadius: '4px',
+						padding: '1rem',
+						marginBottom: '1rem',
+						overflowY: 'auto'
+					}}>
+				</div>
+				
+				<MessageForm />
+			</div>
+		);
 	};
 };
--- src/components/ChatPage/SidePanel/index.jsx
+++ src/components/ChatPage/SidePanel/index.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import UserPanel from './UserPanel';
+import Favorited from './Favorited';
+import ChatRooms from './ChatRooms';
+import DirectMessages from './DirectMessages';
 
 function SidePanel() {
-	return <div>SidePanel</div>;
+	return (
+		<div
+			style={{
+				backgroundColor: '#7B83EB',
+				padding: '2rem',
+				minHeight: '100vh',
+				color: 'white',
+				minWidth: '275px'
+			}}
+		>
+		
+			<UserPanel />
+		
+			<Favorited />
+		
+			<ChatRooms />
+		
+			<DirectMessages />
+		
+		SidePanel
+		</div>
+	);
 };
 
 export default SidePanel;
