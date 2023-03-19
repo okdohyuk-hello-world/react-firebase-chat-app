de17d9d0017d1023fe97ff6c27cf6199ea2e0fe8
Dohyuk You <root@okdohyuk.dev>
2023-03-19 14:01:35 +0000

    feat upload image from message form

--- src/components/ChatPage/MainPanel/MessageForm.jsx
+++ src/components/ChatPage/MainPanel/MessageForm.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { db } from '../../../assets/firebase';
+import { db, storage } from '../../../assets/firebase';
 import { ref, serverTimestamp, push, child } from 'firebase/database';
+import { ref as sRef, uploadBytes } from 'firebase/storage';
 import { useSelector } from 'react-redux';
+import mime from 'mime-types';
 
 function MessageForm() {
   const chatRoom = useSelector(state => state.chatRoom.currentChatRoom);
@@ -14,9 +16,27 @@ function MessageForm() {
   const [errors, setErrors] = useState([]);
   const [loading, setLoading] = useState(false);
   const messagesRef = ref(db, 'messages');
+  const inputOpenImageRef = useRef();
 
   const handleContentChange = e => {
     setContent(e.target.value);
+  };
+
+  const handleOpenImageRef = () => {
+    inputOpenImageRef.current.click();
+  };
+
+  const handleUploadImage = async e => {
+    const file = e.target.files[0];
+    const filePath = `/message/public/${file.name}`;
+    const metadata = { contentType: mime.lookup(file.name) };
+    const storageRef = sRef(storage, filePath);
+
+    try {
+      await uploadBytes(storageRef, file, metadata);
+    } catch (e) {
+      alert(e);
+    }
   };
 
   const createMessage = (fileUrl = null) => {
@@ -85,11 +105,23 @@ function MessageForm() {
           </button>
         </Col>
         <Col>
-          <button className="message-form-button" style={{ width: '100%' }}>
+          <button
+            onClick={handleOpenImageRef}
+            className="message-form-button"
+            style={{ width: '100%' }}
+          >
             UPLOAD
           </button>
         </Col>
       </Row>
+
+      <input
+        ref={inputOpenImageRef}
+        style={{ display: 'none' }}
+        type="file"
+        accept="image/jpeg, image/png"
+        onChange={handleUploadImage}
+      />
     </div>
   );
 }
