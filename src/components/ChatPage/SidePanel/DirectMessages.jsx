0c61ca500bf700dacdc7bb6639798e1082a43a09
Dohyuk You <root@okdohyuk.dev>
2023-03-21 13:18:32 +0000

    feat direct messages component style

--- src/components/ChatPage/SidePanel/DirectMessages.jsx
+++ src/components/ChatPage/SidePanel/DirectMessages.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaRegSmile } from 'react-icon/fa';
 
-function DirectMessages() {
-  return <div>DirectMessages</div>;
+class DirectMessages extends Component {
+  state = {};
+
+  renderDirectMessages = () => {};
+
+  render() {
+    return (
+      <div>
+        <span style={{ display: 'flex', alignItems: 'center' }}>
+          <FaRegSmile style={{ marginRight: 3 }} /> DIRECT MESSAGES(1)
+        </span>
+
+        <ul style={{ listStyleType: 'none', padding: 0 }}>{this.renderDirectMessages()}</ul>
+      </div>
+    );
+  }
 }
 
 export default DirectMessages;
