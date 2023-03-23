1182a14aca1c74f95820416a813045d083fae68a
Dohyuk You <root@okdohyuk.dev>
2023-03-23 13:43:36 +0000

    feat view posts count in main panel

--- src/components/ChatPage/MainPanel/MessageHeader.jsx
+++ src/components/ChatPage/MainPanel/MessageHeader.jsx
@@ -20,6 +20,7 @@ function MessageHeader({ handleSearchChange }) {
   const [isFavorited, setIsFavorited] = useState(false);
   const userRef = ref(db, 'users');
   const user = useSelector(state => state.user.currentUser);
+  const userPosts = useSelector(state => state.chatRoom.userPosts);
 
   useEffect(() => {
     if (chatRoom && user) addFavoriteListener(chatRoom.id, user.uid);
@@ -53,6 +54,26 @@ function MessageHeader({ handleSearchChange }) {
         .catch(e => console.error(e));
     }
   };
+
+  const renderUserPosts = userPosts =>
+    Object.entries(userPosts)
+      .sort((a, b) => b[1].count - a[1].count)
+      .map(([key, val], i) => (
+        <Card key={i} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <img
+            style={{ borderRadius: 25, marginLeft: 16 }}
+            width={48}
+            height={48}
+            className="mr-3"
+            src={val.image}
+            alt={val.name}
+          />
+          <Card.Body>
+            <h6>{key}</h6>
+            <p style={{ margin: 0 }}>{val.count}</p>
+          </Card.Body>
+        </Card>
+      ));
 
   return (
     <div
@@ -121,10 +142,12 @@ function MessageHeader({ handleSearchChange }) {
             <Accordion>
               <Card>
                 <Card.Header>
-                  <Accordion.Button eventKey="0">123</Accordion.Button>
+                  <Accordion.Button eventKey="0">Posts Count</Accordion.Button>
                 </Card.Header>
                 <Accordion.Body eventKey="0">
-                  <Card.Body>Hello! I'm the body</Card.Body>
+                  <Card.Body style={{ display: 'flex', flexDirection: 'column', gap: 16 }}>
+                    {userPosts && renderUserPosts(userPosts)}
+                  </Card.Body>
                 </Accordion.Body>
               </Card>
             </Accordion>
